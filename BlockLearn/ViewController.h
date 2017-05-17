63fd86fc712489da3c5a00c668a1d89cdf31c398
WangPu <wangpu@WangPu.local>
2017-05-17 19:43:22 +0800

    项目初始化

---
+++ BlockLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockLearn
+//
+//  Created by WangPu on 2017/5/17.
+//  Copyright © 2017年 WangPU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlockLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlockLearn
+//
+//  Created by WangPu on 2017/5/17.
+//  Copyright © 2017年 WangPU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
